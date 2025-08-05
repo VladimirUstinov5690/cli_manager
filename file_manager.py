6d976922615d3b21df5b5dbff215cd07b8c114b9
Vladimir <ustinov5690@mail.ru>
2025-08-05 12:27:00 +0600

    initial method delete

--- file_manager.py
+++ file_manager.py
@@ -21,23 +21,23 @@ class FileManager:
         
         if os.path.dirname(path_file) == dest_path or dest_path is None:
             dest_path = os.path.dirname(path_file)
-
+        
         path_file = os.path.abspath(path_file)
-
+        
         if dest_path is None or os.path.dirname(path_file) == dest_path:
             dest_path = os.path.dirname(path_file)
-
+        
         filename = os.path.basename(path_file)
         create_path = os.path.join(dest_path, filename)
-
+        
         if os.path.isfile(create_path):
             create_path = change_file_name(filename, dest_path)
-
+        
         shutil.copy(path_file, create_path)
         print(f'Файл {filename} успешно скопирован в {create_path}')
         return create_path
-
-
-
-
-
+    
+    @staticmethod
+    def delete(path):
+        """Удаляет указанную директорию или файл"""
+        pass
